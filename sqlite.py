c075eacf06d2f649f4aa8d3cded82bb2ebeef97e
Phil Veloso <philveloso@PenguinPirate.lan>
2019-07-25 00:05:45 +0100

    Py : sqlite - seperate functions and improve logging

--- sqlite.py
+++ sqlite.py
@@ -13,42 +13,72 @@ import monitor
 logger 			= logging.getLogger('monitor')
 
 #----------------------------------------------------------------------
-def init_database(timeStamp):
-	"""
-	Initiaite Database
+def create_connection(db_file):
+	""" 
+	create a database connection to the SQLite database
 	"""
 	try:
-		conn = sqlite3.connect('database.db')
-		c = conn.cursor()
-
-		c.execute('''CREATE TABLE ''' + config.DOMAIN + '''_sitemap (
-						sequenceNo INTEGER,
-						sitemap TEXT,
-						dateTime INTEGER,
-						totalUrls INTEGER,
-						2xxResponses INTEGER,
-						3xxResponses INTEGER,
-						4xxResponses INTEGER,
-						5xxResponses INTEGER,
-						slowest REAL,
-						average REAL,
-						fastest REAL,
-						totalTime REAL
-					)''')
-
-		c.execute('''CREATE TABLE ''' + config.DOMAIN + '''_urls_''' + timeStamp + ''' (
-			sequenceNo INTEGER,
-			pageUrl TEXT,
-			statusCode INTEGER,
-			loadTime REAL,
-			comment TEXT
-		)''')
-
-		conn.commit()
-		conn.close()
+		conn = sqlite3.connect(db_file)
+		return conn
 	except Exception as e:
-		logger.error( 'Failed : init_database - {0}'.format(e) )
-		monitor.email_send('Script Error', e)
+		logger.error( 'Failed : create_connection - {0}'.format(e) )
+
+	return None
 
 
+#----------------------------------------------------------------------
+def create_table(conn, create_table_sql):
+	""" 
+	create a table
+	"""
+	try:
+		c = conn.cursor()
+		c.execute(create_table_sql)
+	except Exception as e:
+		logger.error( 'Failed : create_table - {0}'.format(e) )
 
+
+#----------------------------------------------------------------------
+def database_init():
+	""" 
+	Initialise database
+	"""	
+	database = config.DB_PATH
+
+	domain_table_name = config.DOMAIN + '_sitemap'
+	url_table_name = config.DOMAIN + '_url'
+	
+	sql_create_domain_table = 	'''CREATE TABLE IF NOT EXISTS {name} (
+									id integer PRIMARY KEY,
+									sitemap_url text NOT NULL,
+									date_time integer NOT NULL,
+									total_urls integer NOT NULL,
+									successes integer,
+									redirects integer,
+									client_errors integer,
+									server_errors integer,
+									slowest real NOT NULL,
+									average real NOT NULL,
+									fastest real NOT NULL,
+									total_ime real NOT NULL
+									);'''.format(name=domain_table_name )
+ 
+	sql_create_url_table = 	'''CREATE TABLE IF NOT EXISTS {name} (
+								id integer PRIMARY KEY,
+								sitemap_id integer,
+								page_url text NOT NULL,
+								status_code integer NOT NULL,
+								load_time real NOT NULL,
+								comment text,
+								FOREIGN KEY (sitemap_id) REFERENCES {domain} (id)
+								)'''.format(name=url_table_name, domain=domain_table_name )
+ 
+    # create a database connection
+	conn = create_connection(database)
+	if conn is not None:
+		# create domain table
+		create_table(conn, sql_create_domain_table)
+		# create url table
+		create_table(conn, sql_create_url_table)
+	else:
+		logger.error( 'Failed : cannot create the database connection' )
