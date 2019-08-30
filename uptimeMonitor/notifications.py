e92d7cabc855731b0ea58bce4a715962500bbe66
Phil Veloso <philveloso@PenguinPirate.local>
2019-08-30 17:41:27 +0100

    Py : notifications - convert to class and commenting

--- uptimeMonitor/notifications.py
+++ uptimeMonitor/notifications.py
@@ -1,31 +1,40 @@
 import requests 	# Used to retrieve sitemail + send mail
-
 import logging 		# Used to record errors
-from logging.handlers import RotatingFileHandler # Used for log rotation
 
 #----------------------------------------------------------------------
 
-import config
+from config import config		# app configuration
 
 #----------------------------------------------------------------------
 
 logger 				= logging.getLogger('monitor')
 
+#----------------------------------------------------------------------	
+
+class Notifications:
+	"""
+	Notifications functions 
+	"""
+
+	def send(subject, body):
+		#----------------------------------------------------------------------		
+		# Retrieve row count from domain table
+		# @return: last row id
+		#----------------------------------------------------------------------
+		try:
+			return requests.post(
+				config.MAILGUN_URL,
+				auth=('api', config.MAILGUN_KEY),
+				data={
+					'from': 'Notification <' +  config.MAILGUN_EMAIL + '>',
+					'to': [config.NOTIFICATION_EMAIL],
+					'subject': subject,
+					'text' : body
+					}
+				)
+		except Exception as e:
+			logger.error( 'Failed : email_send - {0}'.format(e) )
+
+
+# End Notifications Class
 #----------------------------------------------------------------------
-def email_send(subject, body):
-	"""
-	Send email w/ Mailgun
-	"""
-	try:
-		return requests.post(
-			config.MAILGUN_URL,
-			auth=('api', config.MAILGUN_KEY),
-			data={
-				'from': 'Notification <' +  config.MAILGUN_EMAIL + '>',
-				'to': [config.NOTIFICATION_EMAIL],
-				'subject': subject,
-				'text' : body
-				}
-			)
-	except Exception as e:
-		logger.error( 'Failed : email_send - {0}'.format(e) )
